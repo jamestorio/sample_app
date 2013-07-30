daf13ed75918b1ee6aed28066e015e1450a29a7a
James Torio <jamestorio@yahoo.com>
2013-07-30 14:23:15 -0400

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,12 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-
+RSpec.configure do |config|
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
+config.include Capybara::DSL
+end
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
